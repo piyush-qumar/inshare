c4d6c2b72289870b9ff4bf0cb537576495e62ca5
piyush kumar <piyushkumar5271@gmail.com>
2022-03-13 18:22:49 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const cors=require('cors');
 
 const PORT=process.env.PORT||3000;
 app.use(express.static('public'));
@@ -8,6 +9,15 @@ app.use(express.json());
 
 const connectDB =require('./config/db');
 connectDB ();
+
+const corsOptions ={
+    origin: process.env.ALLOWED_CLIENTS.split(',') 
+   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+   app.use(cors(corsOptions));
+
+//var videos = require('./routes/index');
+
 
 // Template engine
 app.set('views', path.join(__dirname, '/views'));
@@ -17,6 +27,7 @@ app.set('view engine', 'ejs');
 app.use('/api/files',require('./routes/files'));
 app.use('/files',require('./routes/show'));
 app.use('/files/download',require('./routes/download'));
+//app.use('/vids', videos);
 
 
 app.listen(PORT,()=>{
